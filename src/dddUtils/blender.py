e074d24b6d6c3e8fc9fbcdfad62102e7fdc69367
Anders Hoff <inconvergent@gmail.com>
2015-11-06 11:58:23 +0100

    move correctly. cloud obj w spheres

--- src/dddUtils/blender.py
+++ src/dddUtils/blender.py
@@ -8,8 +8,23 @@ class Base(object):
 
     self.fn = fn
     self.obj_name = obj_name
+    self.obj = self._import(fn)
 
     return
+
+  def _import(self, fn):
+
+    bpy.ops.object.select_all(action='DESELECT')
+
+    bpy.ops.import_scene.obj(
+      filepath=fn,
+      use_smooth_groups=False,
+      use_edges=True,
+    )
+
+    obj = bpy.context.selected_objects[0]
+
+    return obj
 
   def get_vertex_color(self):
 
@@ -58,19 +73,35 @@ class Base(object):
 
     print(num, numv, len(col.data), i)
 
-  def move_rescale(self, pos, scale):
-
-    bpy.ops.object.origin_set(type='GEOMETRY_ORIGIN')
+  def move_rescale(self, set_pivot=False, pos=False, scale=False, to_origin=False):
 
     obj = self.obj
 
-    sx,sy,sz = obj.scale
+    if set_pivot:
+      bpy.context.scene.cursor_location = set_pivot
+      bpy.ops.object.origin_set(type='ORIGIN_CURSOR')
 
-    sx *= scale
-    sy *= scale
-    sz *= scale
+    if to_origin:
+      bpy.ops.object.origin_set(type='GEOMETRY_ORIGIN')
 
-    obj.scale = ((sx,sy,sz))
+    elif pos:
+      mx,my,mz = obj.location
+
+      mx = pos[0]
+      my = pos[1]
+      mz = pos[2]
+      obj.location = ((mx,my,mz))
+
+    if scale:
+      sx,sy,sz = obj.scale
+      sx *= scale
+      sy *= scale
+      sz *= scale
+      obj.scale = ((sx,sy,sz))
+
+  def set_smooth_shade(self):
+
+    bpy.ops.object.shade_smooth()
 
   def smooth(self, view_levels=1, render_levels=2):
 
@@ -79,8 +110,6 @@ class Base(object):
     bpy.ops.object.modifier_add(type='SUBSURF')
     self.obj.modifiers['Subsurf'].levels = view_levels
     self.obj.modifiers['Subsurf'].render_levels = render_levels
-
-    bpy.ops.object.shade_smooth()
 
   def __set_vis(self, frame, vis=True):
 
@@ -107,9 +136,9 @@ class Base(object):
     self.__set_vis(ain, True)
     self.__set_vis(aout, False)
 
-  def apply_mat(self):
+  def apply_mat(self, name='Material'):
 
-    mat = bpy.data.materials["Material"]
+    mat = bpy.data.materials[name]
     self.obj.data.materials.append(mat)
 
 class Obj(Base):
@@ -117,125 +146,39 @@ class Obj(Base):
   def __init__(self, fn, obj_name):
 
     Base.__init__(self, fn, obj_name)
-    self.obj = self.__import(fn)
-
-  def __import(self, fn):
-
-    bpy.ops.object.select_all(action='DESELECT')
-
-    bpy.ops.import_scene.obj(
-      filepath=fn,
-      use_smooth_groups=False,
-      use_edges=True,
-    )
-
-    obj = bpy.context.selected_objects[0]
-
-    return obj
-
-#class CloudObj(object):
-
-  #def __init__(self, fn, obj_name):
-
-    #self.fn = fn
-    #self.obj_name = obj_name
-    #self.obj = self.__import(fn)
-
-    #return
-
-  #def __import(self, fn):
-
-    #bpy.ops.object.select_all(action='DESELECT')
-
-    #bpy.ops.import_scene.obj(
-      #filepath=fn,
-      #use_smooth_groups=False,
-      #use_edges=True,
-    #)
-
-    #obj = bpy.context.selected_objects[0]
-
-    #return obj
 
 
-  #def del_mesh(self):
+class Cloud(Base):
 
-    #bpy.ops.object.select_all(action='DESELECT')
-    #self.obj.select=True
-    #bpy.ops.object.delete()
+  def __init__(self, fn, obj_name):
 
-  #def spheres(self):
+    Base.__init__(self, fn, obj_name)
 
-    #from numpy import array
+  def spheres(self, scale=0.5, mat='Material'):
+
     #from numpy import row_stack
-    #from numpy import diff
-    #from numpy import mean
-    #from numpy.linalg import norm
-    #from collections import defaultdict
-
-    #base_scale = 0.65
-
     #scn = bpy.context.scene
-    #obj = self.obj
     #world = obj.matrix_world
-    #mat = bpy.data.materials["Material"]
 
+    obj = self.obj
+    mat = bpy.data.materials[mat]
 
-    #bpy.ops.surface.primitive_nurbs_surface_sphere_add(
-      #radius = 1,
-      #location = (0,0,0)
-    #)
-    #sphere = bpy.context.active_object
-    #sphere.data.materials.append(mat)
-    #bpy.context.active_object.hide = True
-    #bpy.context.active_object.hide_render = True
+    bpy.ops.surface.primitive_nurbs_surface_sphere_add(
+      radius = 1,
+      location = (0,0,0)
+    )
+    sphere = bpy.context.active_object
 
-    #mesh = sphere.data
+    sx,sy,sz = sphere.scale
+    sx *= scale
+    sy *= scale
+    sz *= scale
 
-    #vertex_map = defaultdict(list)
-    #vertices = obj.data.vertices
-    #edges = obj.data.edges
-    #vnum = len(vertices)
-    #enum = len(edges)
-    #vert_co = row_stack([world*v.co for v in vertices])
+    sphere.scale = ((sx,sy,sz))
 
-    #print('\ncalculating sizes:\n')
+    sphere.data.materials.append(mat)
 
-    #for i,e in enumerate(edges):
+    sphere.parent = obj
 
-      #vv = array([v for v in e.vertices],'int')
-      #dx = diff(vert_co[vv,:],axis=0).squeeze()
-      ##dx = diff(row_stack([ \for v in vv]),axis=0).squeeze()
-      #dd = norm(dx)
-
-      #vertex_map[vv[0]].append(dd)
-      #vertex_map[vv[1]].append(dd)
-
-      #if i%100==0:
-        #print(i, enum)
-
-    #vertex_weight = {k:mean(d) for k,d in vertex_map.items()}
-
-    #print('\nplacing objects:\n')
-
-    #for i,(v,rad) in enumerate(vertex_weight.items()):
-
-      #o = bpy.data.objects.new('one', mesh)
-      ##o.location = world*vertices[v].co
-      #o.location = vert_co[v,:]
-
-      #scale = rad*base_scale
-
-      #sx,sy,sz = o.scale
-      #sx *= scale
-      #sy *= scale
-      #sz *= scale
-
-      #o.scale = ((sx,sy,sz))
-      #scn.objects.link(o)
-
-      #if i%100==0:
-        #print(i, vnum)
-
-
+    obj.dupli_type = 'VERTS'
 
