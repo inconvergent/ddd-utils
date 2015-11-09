cdceb3d211df8757a89cae083d77af2d4d1874b0
Anders Hoff <inconvergent@gmail.com>
2015-11-09 10:18:33 +0100

    get_mid_2d

--- src/dddUtils/ddd.py
+++ src/dddUtils/ddd.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
 
+def get_mid_2d(v):
+
+  from numpy import array
+
+  mi = v.min(axis=0).squeeze()
+  ma = v.max(axis=0).squeeze()
+  midx = (mi[0]+ma[0])*0.5
+  midy = (mi[1]+ma[1])*0.5
+  move = array([[midx,midy]])
+  return move
+
 def random_unit_vec(num, scale):
 
   from numpy.random import normal
@@ -9,6 +20,5 @@ def random_unit_vec(num, scale):
   rnd = normal(size=(num,3))
   d = norm(rnd,axis=1)
   rnd[:] /= reshape(d, (num,1))
-
   return rnd*scale
 
