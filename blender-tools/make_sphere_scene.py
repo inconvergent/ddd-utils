f08adc831ab4f9718fa591c58ccd92a937474f1f
Anders Hoff <inconvergent@gmail.com>
2015-11-09 12:59:10 +0100

    add sphere scene script

---
+++ blender-tools/make_sphere_scene.py
@@ -0,0 +1,40 @@
+import bpy
+
+
+def main(argv):
+
+  from time import time
+  from dddUtils.blender import Cloud
+
+  fn = argv[0]
+  fn_out = argv[1]
+
+  try:
+    scale = argv[2]
+  except Exception:
+    scale = 1.0
+
+  print('importing: ' + fn)
+
+  t1 = time()
+
+  C = Cloud(fn,'a')
+  C.move_rescale(set_pivot=[0.5,-0.5,0.5], pos=[0,0,0], scale=scale)
+  C.spheres(scale=0.0005)
+
+  print('\ntime:',time()-t1,'\n\n')
+
+  # bpy.data.scenes["Scene"].render.filepath = img_out
+  # bpy.ops.render.render(write_still=True)
+
+  bpy.ops.wm.save_as_mainfile(filepath=fn_out)
+
+
+if __name__ == '__main__':
+
+  import sys
+  argv = sys.argv
+
+  argv = argv[argv.index("--") + 1:]
+  main(argv)
+
