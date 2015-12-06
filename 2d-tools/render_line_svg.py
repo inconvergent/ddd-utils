a39d49716306c95b7f68cc517b9f002ec63735d4
Anders Hoff <inconvergent@gmail.com>
2015-12-06 20:44:12 +0100

    .

--- 2d-tools/render_line_svg.py
+++ 2d-tools/render_line_svg.py
@@ -7,38 +7,30 @@ BLACK = [0,0,0,1]
 def make_line(c, vertices, edges, n=10):
 
   from numpy import arange
+  from numpy import array
   from numpy.random import random
   from numpy import roll
 
-  rolls = 3
   xys = vertices[edges]
 
   ii = arange(n)/float(n)
+
+  c.new_path()
 
   for xy in xys:
 
     start = xy[0,:]
     stop = xy[1,:]
     dd = (stop-start)*random()
+
     c.move_to(*start)
     c.line_to(*(start+dd))
 
-    # vc = xy[2,:]
-    
-    # v1 = va-vc
-    # v2 = vb-vc
-
-    # for i in ii:
-
-      # start = va - i*v1
-      # stop = vb - i*v2
-      # dd = (stop-start)*random()
-
-      # c.move_to(*start)
-      # c.line_to(*start+dd)
-
+    # c.move_to(*start)
+    # c.line_to(*(start+dd[::-1]*array([-1,1])))
+  
   c.stroke()
-
+  
   return
 
 def main(args, **argv):
@@ -72,11 +64,8 @@ def main(args, **argv):
     data = load(fn)
 
     vertices = data['vertices']
+    vertices *= scale*size
     edges = data['edges']
-
-    vertices *= scale
-    vertices *= size
-
     make_line(c, vertices, edges)
 
   c.save()
