74cdd5233db0d0805c597c393f799e65920e9dcb
Anders Hoff <inconvergent@gmail.com>
2015-12-29 21:17:05 +0100

    bug in random

--- src/dddUtils/random.py
+++ src/dddUtils/random.py
@@ -5,11 +5,15 @@ from __future__ import print_function
 
 from numpy.random import normal
 from numpy.random import random
+from numpy.linalg import norm
+from numpy import array
+from numpy import pi as PI
+from numpy import cos
+from numpy import sin
 
 
 def random_unit_vec(num, scale):
 
-  from numpy.linalg import norm
   from numpy import reshape
 
   rnd = normal(size=(num,3))
@@ -17,56 +21,50 @@ def random_unit_vec(num, scale):
   rnd[:] /= reshape(d, (num,1))
   return rnd*scale
 
-def random_points_in_circle(n, x, y, rad):
+def random_points_in_circle(n,xx,yy,rr):
   """
   get n random points in a circle.
   """
 
-  from numpy import zeros, logical_not
+  from numpy import zeros
+  from numpy import logical_not
   from numpy import column_stack
-  from numpy import cos
-  from numpy import sin
-  from numpy import array
-  from numpy import pi
   from numpy import reshape
 
   rnd = random(size=(n,3))
-  t = 2.*pi*rnd[:,0]
+  t = 2.*PI*rnd[:,0]
   u = rnd[:,1:].sum(axis=1)
   r = zeros(n,'float')
   mask = u>1.
   xmask = logical_not(mask)
   r[mask] = 2.-u[mask]
   r[xmask] = u[xmask]
-  xyp = reshape(rad*r,(n,1))*column_stack( (cos(t),sin(t)) )
-  dartsxy  = xyp + array([x,y])
+  xyp = reshape(rr*r,(n,1))*column_stack( (cos(t),sin(t)) )
+  dartsxy  = xyp + array([xx,yy])
   return dartsxy
 
-
-def darts(n, x, y, rad, dst, old_darts=None):
+def darts(n, xx, yy, rr, dst):
   """
   get at most n random, uniformly distributed, points in a circle.
-  centered at (x,y), with radius rr. points are no closer to each other
+  centered at (xx,yy), with radius rr. points are no closer to each other
   than dst.
   """
 
-  from numpy import array
-  from numpy import zeros
-  from numpy import row_stack 
   from scipy.spatial import cKDTree as kdt
 
-  dartsxy = random_points_in_circle(n, x, y, rad)
-  jj = zeros(n,'bool')
+  ## remove new nodes that are too close to other
+  ## new nodes
 
+  visited = set()
+  dartsxy = random_points_in_circle(n, xx, yy, rr)
   tree = kdt(dartsxy)
-  for j,near in enumerate(tree.query_ball_point(dartsxy, dst)):
-    if len(near)<2:
-      jj[j] = True
+  near = tree.query_ball_point(dartsxy, dst)
+  jj = []
+  for j,n in enumerate(near):
+
+    if len(visited.intersection(n))<1:
+      jj.append(j)
+      visited.add(j)
 
   res = dartsxy[jj,:]
-
-  if old_darts is not None:
-    return row_stack([old_darts, res])
-
   return res
-
